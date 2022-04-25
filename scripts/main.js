90bab02ac3b9b5b6040918d301ad7dcd7ebd241b
Tim Dettinger <6506703@isd535.org>
2022-04-25 11:33:42 -0500

    bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 (function (window) {
     'use strict'; 
-
+    
     const FORM_SELECTOR = '[data-coffee-order="form"]';
-    const CHECKLIST_SELECTOR ='[ data-coffee-order="form"]';
+    
 
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let RemoteDataStore = App.RemoteDataStore;
     let FormHandler = App.FormHandler;
-    let CheckList = App.CheckList;
+    
     let Validation = App.Validation;
 
     var myTruck= new Truck('12345', new RemoteDataStore("http://saturn.rochesterschools.org:8080/json"));
@@ -17,13 +17,13 @@
 
     let formHandler = new FormHandler(FORM_SELECTOR);
 
-    let checkList = new CheckList(CHECKLIST_SELECTOR);
+    
 
-    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    
 
     formHandler.addSubmitHandler(function (data) {
         myTruck.createOrder.call(myTruck, data);
-        checkList.addRow.call(checkList, data);
+        
     });
 
     formHandler.addInputHandler(Validation.isCompanyEmail);
